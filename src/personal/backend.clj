e67761710607ee8adca4f777c3a2e1e86102a496
adham-omran <git@adham-omran.com>
2024-01-19 07:20:45 +0300

    Update personal.backend

--- src/personal/backend.clj
+++ src/personal/backend.clj
@@ -1,53 +1,82 @@
 (ns personal.backend
-  (:require [reitit.ring :as ring]
-            [ring.adapter.jetty :as adapter]
-            ))
+  (:require
+   [clojure.java.io :as io]
+   [hiccup.page]
+   [reitit.dev.pretty :as pretty]
+   [reitit.ring.malli]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.middleware.exception :as exception]
+   [reitit.coercion.malli]
+   [muuntaja.core :as m]
+   [reitit.ring :as ring]
+   [reitit.ring.coercion :as coercion]
+   [reitit.ring.middleware.multipart :as multipart]
+   [reitit.ring.middleware.parameters :as parameters]
+   [ring.adapter.jetty :as adapter]))
+
+(def some-data {:x 1
+                :y 2
+                :z 3})
 
 (defn handler [_]
-  {:status 200, :body "ok"})
+  {:status 200
+   :body (hiccup.page/html5 [:p. (str some-data)])})
 
 (defn wrap [handler id]
   (fn [request]
     (update (handler request) :wrap (fnil conj '()) id)))
 
-(comment
-  (ring/ring-handler
-   (ring/router
-    ["/"
-     ["home" {:get ""}]
-     ["api" {:middleware [[wrap :api]]}
-      ["/ping" {:get handler
-                :name ::ping}]
-      ["/admin" {:middleware [[wrap :admin]]}
-       ["/users" {:get handler
-                  :post handler}]]]
-     ["assets/*" (ring/create-resource-handler)]]
-    )
-   (ring/create-default-handler)))
-
-
 (def app
   (ring/ring-handler
    (ring/router
-    ["/"
-     ["home" {:get
-              {:handler (fn [_] {:body (slurp "resources/assets/index.html")
-                                 :headers {"Content-Type" "text/html"}})}}]
-     ["clicked" {:post
-                 {:handler (fn [_] {:body (slurp "resources/assets/response.html")
-                                 :headers {"Content-Type" "text/html"}})}}]
-     ["api" {:middleware [[wrap :api]]}
-      ["/ping" {:get handler
-                :name ::ping}]
-      ["/admin" {:middleware [[wrap :admin]]}
-       ["/users" {:get handler
-                  :post handler}]]]
-     ["assets/*" (ring/create-resource-handler)]]
-    )
-   (ring/create-default-handler))
-  )
+    [["/" (constantly {:status 200
+                       :body (slurp (io/resource "./public/index.html"))})]
+     ["/trigger" {:get {#_:headers #_{"Content-Type" "text/html"}
+                        :coercion reitit.coercion.malli/coercion
+                        :handler (fn [req]
+                                   (println "hello")
+                                   (println {:request req})
+                                   {:body (hiccup.page/html5 [:div "RESULTS"])})
+                        :parameters {:query [:map [:q string?]]}}}]
+     ["/api/ping" {:post {:handler handler}}]
+     ["/plus"
+      {:get {:summary "plus with malli query parameters"
+             :coercion reitit.coercion.malli/coercion
+             :parameters {:query [:map
+                                  [:y int?]]}
+             :responses {200 {:body [:map [:total int?]]}}
+             :handler (fn [req]
+                        (println req)
+                        {:status 200
+                         :body {:total (:query-params req)}})}}]
+     ["/*" (ring/create-resource-handler)]]
 
+    {:exception pretty/exception
+     :muuntaja m/instance
+     :conflicts (constantly nil)
+     :middleware [;; query-params & form-params
+                  parameters/parameters-middleware
+                           ;; content-negotiation
+                  muuntaja/format-negotiate-middleware
+                           ;; encoding response body
+                  muuntaja/format-response-middleware
+                           ;; exception handling
+                  exception/exception-middleware
+                           ;; decoding request body
+                  muuntaja/format-request-middleware
+                           ;; coercing response bodys
+                  coercion/coerce-response-middleware
+                           ;; coercing request parameters
+                  coercion/coerce-request-middleware
+                           ;; multipart
+                  multipart/multipart-middleware]})
+   (ring/routes (ring/create-default-handler))))
 
-(def server
-  (adapter/run-jetty #'app {:port 8090
-                            :join? false}))
+(comment
+  (app {:request-method :get
+        :uri "/trigger"
+        :query-params {:q "foo"}})
+  (def server
+    (adapter/run-jetty #'app {:port 8090
+                              :join? false}))
+  (.stop server))
